7bcaa8ab1dcd0802ce468dc77bf07e9c35cfe229
Hatatori Nitsukawa <kcireh.i@gmail.com>
2019-09-21 14:40:32 -0300

    Update japones.js

--- japones.js
+++ japones.js
@@ -1,12 +1,10 @@
 // t = "ka,ki,ku,ke,ko,sa,si,shi,su,se,so,za,zi,zu,dzu,ze,zo,ta,ti,tu,tsu,te,to,da,di,ji,dzu,de,do,na,ni,nu,ne,no,ha,hi,hu,he,ho,ba,bi,bu,be,bo,pa,pi,pu,pe,po,ma,mi,mu,me,mo,ya,yu,yo,wa,wo,n".split(",")
 
-t = "ka ki ku ke ko sa si shi su se so za zi zu dzu ze zo ta ti chi tu tsu te to da di ji du dzu de do na ni nu ne no ha hi hu he ho ba bi bu be bo pa pi pu pe po ma mi mu me mo ya yu yo wa wo n".split(" ")
-hir = "かきくけこさししすせそざじずずぜぞたちちつつてとだぢぢづづでどなにぬねのはひふへほばびぶべぼぱぴぷぺぽまみむめもやゆよわをん".split("")
-kan = "カキクケコサシシスセソザジズズゼゾタチチツツテトダヂヂヅヅデドナニヌネノハヒフヘホバビブベボパピプペポマミムメモヤユヨワヲン".split("")
+t = "a i u e o ka ki ku ke ko sa si shi su se so za zi zu dzu ze zo ta ti chi tu tsu te to da di ji du dzu de do na ni nu ne no ha hi hu he ho ba bi bu be bo pa pi pu pe po ma mi mu me mo ya yu yo wa wo n".split(" ")
+hir = "あいうえおかきくけこさししすせそざじずずぜぞたちちつつてとだぢぢづづでどなにぬねのはひふへほばびぶべぼぱぴぷぺぽまみむめもやゆよわをん".split("")
+kan = "アイウエオカキクケコサシシスセソザジズズゼゾタチチツツテトダヂヂヅヅデドナニヌネノハヒフヘホバビブベボパピプペポマミムメモヤユヨワヲン".split("")
 
 s = ""
-
-
 
 
 for(i=0;i<100;i++){
